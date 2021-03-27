ab409e173f703a2496ebdca6b3b4a20e8cd3a564
Ahlam-Fuad <78500280+Ahlam-Fuad@users.noreply.github.com>
2021-03-27 23:51:26 +0300

    Add files via upload

---
+++ Empty.java
@@ -0,0 +1,21 @@
+/**
+ * Created by User on 27/03/21.
+ */
+public class Empty {
+    if(isEmpty()) {
+    throws new Empty();
+    }
+    int count = 1;
+    Node hrunner = head;
+    Node trunner = tail;
+while(hrunner!=trunner && hrunner.getNext()!=trunner) {
+        count++; count++;
+        hrunner = hrunner.getNext();
+        trunner = trunner.getNext();
+    }
+if(hrunner!=trunner) {
+        // the pointers don't match - even number of elements
+        count--;
+    }
+
+}
